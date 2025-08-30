4c49d538bf089f40e24d23f5b8c6739b971b3372
Anand <s.anandsiva7897@gmail.com>
2025-08-30 14:25:09 +0530

    updateD

---
+++ src/pages/customerorder/customerorder.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customerorder',
+  imports: [],
+  templateUrl: './customerorder.component.html',
+  styleUrl: './customerorder.component.scss'
+})
+export class CustomerorderComponent {
+
+}
